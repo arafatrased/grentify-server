e2c9ce91b68e101a5cd9091ed9d5a19bd0edf40c
arafatrased <rasedmathku@gmail.com>
2025-04-22 23:20:38 +0600

    client secret are set

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const PORT = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
